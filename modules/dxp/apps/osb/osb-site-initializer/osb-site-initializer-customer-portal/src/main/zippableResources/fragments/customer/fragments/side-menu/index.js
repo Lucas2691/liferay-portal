3302d23dff6b8569ee5c2e8afcbef38814ff17c4
Lucas Soares <lucas.soares@liferay.com>
2021-11-05 16:53:49 -0300

    LPS-140160 Customer Portal Fragment Side Menu

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/src/main/zippableResources/fragments/customer/fragments/side-menu/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/src/main/zippableResources/fragments/customer/fragments/side-menu/index.js
@@ -10,26 +10,124 @@
  * distribution rights of the Software.
  */
 
-fragmentElement.addEventListener("click", (event) => {
-  const items = fragmentElement.querySelectorAll(".menu-item-content");
+const getAccountSubscriptionGroupsByFilter = (filter) => ({
+  query: `{
+      c {
+        accountSubscriptionGroups(filter: "accountKey eq '${filter}' and hasActivation eq true") {
+            items {
+              name
+            }
+          }
+        }
+  }`});
 
-  if (event.target.classList.contains("menu-item-content")) {
-    event.target.classList.toggle("active");
-  }
+const doFetch = async (query) => {
+  const queryString = JSON.stringify(query);
 
-  items.forEach((f) => {
-    if (f.classList.contains("active") != event.target) {
-      if (f != event.target) {
-        f.classList.remove("active");
-      }
-    }
+  const response = await fetch(`${window.location.origin}/o/graphql`, {
+    body: queryString,
+    headers: {
+      'Content-Type': 'application/json',
+      'x-csrf-token': Liferay.authToken,
+    },
+    method: 'POST',
   });
-  if (event.target.id == "subMenu") {
-    const arrow = fragmentElement.querySelector(".arrow");
-    const menu = fragmentElement.querySelector(".sub-menu");
-    arrow.classList.toggle("arrow-left");
-    arrow.classList.toggle("arrow-down");
-    menu.classList.toggle("sub-menu-closed");
-    menu.classList.toggle("sub-menu-open");
+
+  const { data } = await response.json();
+
+  return data.c.accountSubscriptionGroups.items;
+}
+
+const getSubscriptionKey = (name) => {
+  return name.split(" ")[0].toLowerCase();
+}
+
+const htmlElement = (name, key) => {
+  return `<li><a href="#" class="btn-sm text-link-md d-flex text-decoration-none text-neutral-10 pr-0">
+  <img class="mr-2" width="16" src="http://localhost:8080/webdav/cp-3.0/document_library/assets/navigation-menu/${key}_icon_gray.svg" alt="" />
+  ${name}
+</a></li>`;
+}
+
+const buttonStyle = "bg-brand-primary-lighten-5 btn btn-borderless btn-primary btn-sm d-flex font-weight-bolder text-brand-primary py-2 rounded";
+const linkStyle = "btn-sm text-link-md d-flex text-decoration-none text-neutral-10 pr-0";
+
+const koroneikiApplicationIdKey = 'customer-portal-koroneiki-application';
+const koroneikiApplication = sessionStorage.getItem(koroneikiApplicationIdKey);
+const currentProducts = fragmentElement.querySelector("#customer-portal-products");
+let expandedHeightProducts;
+
+(async () => {
+  try {
+    if (koroneikiApplication) {
+      let koroneiki = JSON.parse(koroneikiApplication);
+
+      const accountSubscriptionGroups = await doFetch(getAccountSubscriptionGroupsByFilter(koroneiki.accountKey)) || [];
+      expandedHeightProducts = accountSubscriptionGroups.length * 40;
+
+      currentProducts.innerHTML = accountSubscriptionGroups.map(
+        ({ name }) => htmlElement(name, getSubscriptionKey(name))
+      ).join("\n");
+    }
+  }
+  catch (error) {
+    console.error(error.message);
+  }
+})();
+
+fragmentElement.addEventListener("click", (event) => {
+  if (event.target.id === "customer-portal-toggle-products" || event.target.id === "customer-portal-arrow") {
+    const products = fragmentElement.querySelector("#customer-portal-products");
+    const heightProducts = products.offsetHeight;
+
+    if (heightProducts < expandedHeightProducts) {
+      currentProducts.style.height = `${expandedHeightProducts}px`;
+    } else {
+      currentProducts.style.height = "0px";
+    }
+
+    const arrow = fragmentElement.querySelector("#customer-portal-arrow");
+    arrow.classList.toggle("left");
+    arrow.classList.toggle("down");
+  } else if (event.target.classList.contains("text-link-md") || event.target.tagName === "IMG") {
+    const lastButton = fragmentElement.querySelector(".btn");
+    let currentButton = event.target;
+
+    if (currentButton.tagName === "IMG") {
+      currentButton = currentButton.parentElement;
+    }
+
+    lastButton.className = linkStyle;
+    currentButton.className = buttonStyle;
+
+    if (currentButton.children[0]) {
+      let srcIcon = currentButton.children[0].src;
+      if (srcIcon.substr(srcIcon.length - 3) !== "svg") {
+        srcIcon = srcIcon.split("/");
+        srcIcon.pop();
+        srcIcon = srcIcon.join("/");
+      }
+
+      currentButton.children[0].src = srcIcon.replace("_gray", "");
+    }
+
+    if (lastButton !== currentButton && lastButton.children[0]) {
+      let srcIcon = lastButton.children[0].src;
+
+      if (srcIcon.substr(srcIcon.length - 3) !== "svg") {
+        srcIcon = srcIcon.split("/");
+        srcIcon.pop();
+        srcIcon = srcIcon.join("/");
+      }
+
+      lastButton.children[0].src = srcIcon.replace(".svg", "_gray.svg");
+    }
+
+    const toggleProducts = fragmentElement.querySelector("#customer-portal-toggle-products");
+    const grandParentElementId = currentButton.parentElement.parentElement.id;
+    if ((grandParentElementId === "customer-portal-products" && toggleProducts.classList.contains("text-neutral-10")) || (toggleProducts.classList.contains("text-brand-primary") && grandParentElementId !== "customer-portal-products")) {
+      toggleProducts.classList.toggle("text-neutral-10");
+      toggleProducts.classList.toggle("text-brand-primary");
+    }
   }
 });
